c3120d29e8eadf478847f2c8361affd9ae0aa006
웃기진않아 <dugudada01@naver.com>
2025-05-03 18:53:52 +0900

    Initial commit

---
+++ src/main/java/com/uddangtangtang/UddangtangtangApplication.java
@@ -0,0 +1,13 @@
+package com.uddangtangtang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UddangtangtangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UddangtangtangApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uddangtangtang/UddangtangtangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uddangtangtang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UddangtangtangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
