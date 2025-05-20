22de53d1dc3930f7a79b63cf1633bc7c66b20448
웃기진않아 <dugudada01@naver.com>
2025-05-20 22:36:30 +0900

    FEAT: 여행 유형 테스트 service

---
+++ src/main/java/com/uddangtangtang/domain/travelType/service/TravelTypeService.java
@@ -0,0 +1,48 @@
+package com.uddangtangtang.domain.travelType.service;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.uddangtangtang.domain.travelType.domain.TravelType;
+import com.uddangtangtang.domain.travelType.dto.request.TypeRequest;
+import com.uddangtangtang.domain.travelType.dto.response.TypeResponse;
+import com.uddangtangtang.domain.travelType.repository.TravelTypeRepository;
+import com.uddangtangtang.global.AI.service.AIService;
+import com.uddangtangtang.global.util.AiTypePromptBuilder;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class TravelTypeService
+{
+    private final TravelTypeRepository travelTypeRepository;
+    private final AIService aiService;
+    private final ObjectMapper objectMapper;
+
+    public TypeResponse generateTravelType(TypeRequest request)
+    {
+        String prompt=AiTypePromptBuilder.buildPromptFromRequest(request);
+        String aiRawResponse = aiService.askChatGPT(prompt).block();
+        TypeResponse typeResponse;
+        try {
+            JsonNode jsonNode = objectMapper.readTree(aiRawResponse);
+
+            String code = jsonNode.path("code").asText();
+            String reason = jsonNode.has("reason") ? jsonNode.get("reason").asText() : jsonNode.path("reson").asText(); // 오타 대응
+            String keyWord = jsonNode.has("keyWord") ? jsonNode.get("keyWord").asText() : "";
+
+            TravelType travelType = travelTypeRepository.findTravelTypeByCode(code)
+                    .orElseThrow(()->new IllegalArgumentException("존재하지 않는 유형 코드입니다: " + code));
+
+            return new TypeResponse(code, reason, keyWord,travelType.getImage(),travelType.getType_description(),travelType.getType_name(),travelType.getTrip_recommand());
+        } catch (Exception e) {
+            log.error("AI 응답 파싱 실패: {}", aiRawResponse, e);
+            return new TypeResponse("ERROR", "AI 응답 파싱 실패", "","","","","");
+        }
+
+
+    }
+
+}
