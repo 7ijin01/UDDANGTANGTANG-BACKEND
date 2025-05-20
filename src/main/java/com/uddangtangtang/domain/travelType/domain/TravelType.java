be69cf73b30fbe511ef3a5deef43800dedce9a22
웃기진않아 <dugudada01@naver.com>
2025-05-20 22:35:30 +0900

    FEAT: TravelType 엔티티

---
+++ src/main/java/com/uddangtangtang/domain/travelType/domain/TravelType.java
@@ -0,0 +1,25 @@
+package com.uddangtangtang.domain.travelType.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "TravelType")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TravelType
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int type_id;
+    private String code;//해당 유형 코드 ex)A-A-A-B
+    private String type_name;// 해당 유형 이름
+    private String type_description;//해당 유형 설명
+    private String image;//해당 유형 캐릭터 저장
+    private String trip_recommand;
+}
