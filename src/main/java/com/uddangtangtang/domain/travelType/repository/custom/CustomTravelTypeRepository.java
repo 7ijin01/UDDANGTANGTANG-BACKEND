4b884232a55329d2897c57e077d1020ed9fa566b
웃기진않아 <dugudada01@naver.com>
2025-05-20 22:34:54 +0900

    FEAT: TravelType 커스텀 레포

---
+++ src/main/java/com/uddangtangtang/domain/travelType/repository/custom/CustomTravelTypeRepository.java
@@ -0,0 +1,8 @@
+package com.uddangtangtang.domain.travelType.repository.custom;
+
+import com.uddangtangtang.domain.travelType.domain.TravelType;
+
+public interface CustomTravelTypeRepository
+{
+
+}
---
+++ src/main/java/com/uddangtangtang/domain/travelType/repository/custom/CustomTravelTypeRepositoryImpl.java
@@ -0,0 +1,10 @@
+package com.uddangtangtang.domain.travelType.repository.custom;
+
+import com.uddangtangtang.domain.travelType.domain.TravelType;
+import com.uddangtangtang.domain.travelType.repository.TravelTypeRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class CustomTravelTypeRepositoryImpl implements CustomTravelTypeRepository
+{
+}
