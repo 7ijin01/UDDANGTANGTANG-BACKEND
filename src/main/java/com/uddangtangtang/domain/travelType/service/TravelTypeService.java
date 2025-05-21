12115057e43fb851cb02383e29df80937064e90e
웃기진않아 <dugudada01@naver.com>
2025-05-21 17:33:27 +0900

    fix: 전역 예외 처리

--- src/main/java/com/uddangtangtang/domain/travelType/service/TravelTypeService.java
+++ src/main/java/com/uddangtangtang/domain/travelType/service/TravelTypeService.java
@@ -7,6 +7,8 @@ import com.uddangtangtang.domain.travelType.dto.request.TypeRequest;
 import com.uddangtangtang.domain.travelType.dto.response.TypeResponse;
 import com.uddangtangtang.domain.travelType.repository.TravelTypeRepository;
 import com.uddangtangtang.global.AI.service.AIService;
+import com.uddangtangtang.global.apiPayload.code.status.ErrorStatus;
+import com.uddangtangtang.global.apiPayload.exception.GeneralException;
 import com.uddangtangtang.global.util.AiTypePromptBuilder;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -25,7 +27,6 @@ public class TravelTypeService
     {
         String prompt=AiTypePromptBuilder.buildPromptFromRequest(request);
         String aiRawResponse = aiService.askChatGPT(prompt).block();
-        TypeResponse typeResponse;
         try {
             JsonNode jsonNode = objectMapper.readTree(aiRawResponse);
 
@@ -34,12 +35,18 @@ public class TravelTypeService
             String keyWord = jsonNode.has("keyWord") ? jsonNode.get("keyWord").asText() : "";
 
             TravelType travelType = travelTypeRepository.findTravelTypeByCode(code)
-                    .orElseThrow(()->new IllegalArgumentException("존재하지 않는 유형 코드입니다: " + code));
+                    .orElseThrow(()->new GeneralException(ErrorStatus.CODE_NOT_FOUND));
 
-            return new TypeResponse(code, reason, keyWord,travelType.getImage(),travelType.getType_description(),travelType.getType_name(),travelType.getTrip_recommand());
+            return new TypeResponse(code,
+                    reason,
+                    keyWord,
+                    travelType.getImage(),
+                    travelType.getType_description(),
+                    travelType.getType_name(),
+                    travelType.getTrip_recommand());
         } catch (Exception e) {
-            log.error("AI 응답 파싱 실패: {}", aiRawResponse, e);
-            return new TypeResponse("ERROR", "AI 응답 파싱 실패", "","","","","");
+            log.error("AI 응답 파싱 실패", e);
+            throw new GeneralException(ErrorStatus.AI_PARSE_ERROR);
         }
 
 
