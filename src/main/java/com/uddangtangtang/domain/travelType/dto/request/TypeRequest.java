b56288a2e8e99ad8f11989e9116ea4316352eed2
웃기진않아 <dugudada01@naver.com>
2025-05-22 19:27:13 +0900

    delete: goalAnswer 제거

--- src/main/java/com/uddangtangtang/domain/travelType/dto/request/TypeRequest.java
+++ src/main/java/com/uddangtangtang/domain/travelType/dto/request/TypeRequest.java
@@ -5,6 +5,5 @@ import lombok.Getter;
 public record TypeRequest(
         String planAnswer,
         String energyAnswer,
-        String moneyAnswer,
-        String goalAnswer
+        String moneyAnswer
 ) {}
