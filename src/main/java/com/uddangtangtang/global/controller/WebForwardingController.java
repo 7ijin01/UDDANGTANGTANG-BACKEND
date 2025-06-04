aad64ebcac7bb5a6f52ed1becb8a1fe55d3495c6
웃기진않아 <dugudada01@naver.com>
2025-06-04 14:11:53 +0900

    fix: index/html 포워딩 주석처리

--- src/main/java/com/uddangtangtang/global/controller/WebForwardingController.java
+++ src/main/java/com/uddangtangtang/global/controller/WebForwardingController.java
@@ -1,14 +1,14 @@
-package com.uddangtangtang.global.controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
-public class WebForwardingController {
-
-    @RequestMapping(value = {"/{path:[^\\.]*}", "/**/{path:[^\\.]*}"})
-    public String forwardToIndex() {
-        return "forward:/index.html";
-
-    }
-}
+//package com.uddangtangtang.global.controller;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//
+//@Controller
+//public class WebForwardingController {
+//
+//    @RequestMapping(value = {"/{path:[^\\.]*}", "/**/{path:[^\\.]*}"})
+//    public String forwardToIndex() {
+//        return "forward:/index.html";
+//
+//    }
+//}
